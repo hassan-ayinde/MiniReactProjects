92e52936a0ced273253cf2bc237e8e20cd2f296a
hassan-ayinde <hassantoheebayinde@gmail.com>
2025-01-02 12:08:24 +0100

    Enhance Product Component: Add dynamic border to selected thumbnail images

--- Add_To_Bag/src/components/Product.jsx
+++ Add_To_Bag/src/components/Product.jsx
@@ -1,19 +1,34 @@
-import React from 'react'
+import React, {useState} from 'react'
 import PoloFront from '../assets/images/polo-colored-front.png'
 import PoloBack from '../assets/images/polo_colored-back.png'
 import PoloSide from '../assets/images/polo-colored-side.png'
 
 const Product = () => {
+    const [selectedImage, setSelectedImage] = useState(PoloFront);
+    const [selectedIndex, setSelectedIndex] = useState(0);
+    const handleSelectedImage = (src,index) => {
+        setSelectedImage(src);
+        setSelectedIndex(index);
+    }
   return (
     <div className="product">
         <div className="product-image">
             <div className="product-image-main">
-                <img src={PoloFront} alt="" />
+                <img src={selectedImage} alt="" />
             </div>
             <div className='product-image-thumbnails'>
-                <img src={PoloFront} alt="" />
-                <img src={PoloSide} alt="" />
-                <img src={PoloBack} alt="" />
+                <img src={PoloFront} alt="" 
+                 onClick={() => handleSelectedImage(PoloFront,0)}
+                 className={selectedIndex === 0 ? 'selected' : ''}
+                />
+                <img src={PoloSide} alt="" 
+                 onClick={() => handleSelectedImage(PoloSide,1)}
+                 className={selectedIndex === 1 ? 'selected' : ''}
+                />
+                <img src={PoloBack} alt="" 
+                 onClick={() => handleSelectedImage(PoloBack,2)}
+                 className={selectedIndex === 2 ? 'selected' : ''}
+                />
             </div>
         </div>
         <div className="product-details">

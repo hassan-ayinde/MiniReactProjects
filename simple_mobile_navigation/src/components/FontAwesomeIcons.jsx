b3637c324c305de4796bbbb9e5f19837ec11b6de
hassan-ayinde <hassantoheebayinde@gmail.com>
2025-01-03 20:57:53 +0100

    Create FontAwesomeIcons: Utility file that contains all font awesome icons to be used in this project

---
+++ simple_mobile_navigation/src/components/FontAwesomeIcons.jsx
@@ -0,0 +1,7 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+import { faUsers, faEnvelope, faBell, faLocationDot, faUserGroup,faGear, faAngleRight } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faUsers, faEnvelope, faBell, faLocationDot, faUserGroup,faGear,faAngleRight);
+
+export { FontAwesomeIcon } from '@fortawesome/react-fontawesome';

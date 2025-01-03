b9f30a511862f169a0bc1ad129525df9258ca4de
hassan-ayinde <hassantoheebayinde@gmail.com>
2025-01-03 21:29:03 +0100

    Refactor MenuList Component: Extracting the MenuItem Componenet props into an array to use map method which simplifies the code base

--- simple_mobile_navigation/src/components/MenuList.jsx
+++ simple_mobile_navigation/src/components/MenuList.jsx
@@ -8,75 +8,48 @@ const MenuList = () => {
       <FontAwesomeIcon icon='angle-right'/>
     </span>
 );
+const mainOptions = [
+  { menuOption: 'Personal Data', optionIcon: <FontAwesomeIcon icon="user-group" /> },
+  { menuOption: 'Messages', optionIcon: <FontAwesomeIcon icon="envelope" /> },
+  { menuOption: 'Notifications', optionIcon: <FontAwesomeIcon icon="bell" /> },
+  { menuOption: 'Location', optionIcon: <FontAwesomeIcon icon="location-dot" /> },
+  { menuOption: 'Community', optionIcon: <FontAwesomeIcon icon="users" /> },
+];
+
+const otherOptions = [
+  { menuOption: 'FAQs', optionIcon: <FontAwesomeIcon icon="user-group" /> },
+  { menuOption: 'Settings', optionIcon: <FontAwesomeIcon icon="gear" /> },
+];
   return (
     <div>
       <ul className='main-option-container'>
-        <li>
-          <MenuItem 
-          menuOption='Personal Data'
-          icons={{
-            option: <FontAwesomeIcon icon='user-group'/>,
-            dropdown: defaultDropdownIcon
-          }}
-          />
-        </li>
-        <li>
-          <MenuItem 
-          menuOption='Messages'
-          icons={{
-            option: <FontAwesomeIcon icon='envelope'/>,
-            dropdown: defaultDropdownIcon
-          }}
-          />
-        </li>
-        <li>
-          <MenuItem 
-          menuOption='Notifications'
-          icons={{
-            option: <FontAwesomeIcon icon='bell'/>,
-            dropdown: defaultDropdownIcon
-          }}
-          />
-        </li>
-        <li>
-          <MenuItem 
-          menuOption='Location'
-          icons={{
-            option: <FontAwesomeIcon icon='location-dot'/>,
-            dropdown: defaultDropdownIcon
-          }}
-          />
-        </li>
-        <li>
-          <MenuItem 
-          menuOption='Community'
-          icons={{
-            option: <FontAwesomeIcon icon='users'/>,
-            dropdown: defaultDropdownIcon
-          }}
-          />
-        </li>
+        {mainOptions.map((item,index) => {
+          return (
+            <li key={index}>
+              <MenuItem
+                menuOption={item.menuOption}
+                icons={{
+                  option: item.optionIcon,
+                  dropdown: defaultDropdownIcon,
+                }}
+              />
+            </li>
+          )
+        })}
       </ul>
 
       <ul className='other-option'>
-        <li>
-          <MenuItem 
-          menuOption='FAQs'
-          icons={{
-            option: <FontAwesomeIcon icon='user-group'/>,
-            dropdown: defaultDropdownIcon
-          }}
-          />
-        </li>
-        <li>
-          <MenuItem 
-          menuOption='Settings'
-          icons={{
-            option: <FontAwesomeIcon icon='gear'/>,
-            dropdown: defaultDropdownIcon
-          }}
-          />
-        </li>
+        {otherOptions.map((item, index) => (
+          <li key={index}>
+            <MenuItem
+              menuOption={item.menuOption}
+              icons={{
+                option: item.optionIcon,
+                dropdown: defaultDropdownIcon,
+              }}
+            />
+          </li>
+        ))}
       </ul>
     </div>
   )

94d2d1e2353928bfb979ec2ad7cd9c2cb8162cca
hassan-ayinde <hassantoheebayinde@gmail.com>
2025-01-04 02:07:27 +0100

    Feat: Updating the profile image from local storage and camera

--- simple_mobile_navigation/src/components/FontAwesomeIcons.jsx
+++ simple_mobile_navigation/src/components/FontAwesomeIcons.jsx
@@ -1,7 +1,7 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
 
-import { faUsers, faEnvelope, faBell, faLocationDot, faUserGroup,faGear, faAngleRight, faHouse,faChartLine, faUser} from '@fortawesome/free-solid-svg-icons';
+import { faUsers, faEnvelope, faBell, faLocationDot, faUserGroup,faGear, faAngleRight, faHouse,faChartLine, faUser, faCamera} from '@fortawesome/free-solid-svg-icons';
 
-library.add(faUsers, faEnvelope, faBell, faLocationDot, faUserGroup,faGear,faAngleRight, faHouse, faChartLine,faUser);
+library.add(faUsers, faEnvelope, faBell, faLocationDot, faUserGroup,faGear,faAngleRight, faHouse, faChartLine,faUser, faCamera);
 
 export { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
--- simple_mobile_navigation/src/components/UserProfileCard.jsx
+++ simple_mobile_navigation/src/components/UserProfileCard.jsx
@@ -1,17 +1,54 @@
-import React from 'react'
+import React,{useState,useRef} from 'react'
 import ProfileImage from '../assets/images/cover.png'
+import { FontAwesomeIcon } from './FontAwesomeIcons';
 
+const UserProfileCard = ({setErrorMessage}) => {
+  const [image,setImage] = useState(ProfileImage);
+  const fileInputRef = useRef(null);
 
-const UserProfileCard = () => {
+  const MAX_FILE_SIZE_MB = 1;
+
+  const handleProfileImageContainer = () => {
+    if (fileInputRef.current) fileInputRef.current.click();
+  }
+
+  const handleProfileImageChange = (e) => {
+    const file = e.target.files[0];
+    if (file){
+      const fileSizeMB = file.size / (1024 * 1024);
+
+      if (fileSizeMB > MAX_FILE_SIZE_MB) {
+        setErrorMessage('Please upload an image less than 1MB.');
+        return;
+      }
+
+      const imageUrl = URL.createObjectURL(file);
+      setImage(imageUrl);
+      setErrorMessage(''); // Clear error if the upload is successful
+    }
+  }
   return (
     <div className='userprofile-card'>
-        <div>
-            <img src={ProfileImage} alt="" />
+      <div onClick={handleProfileImageContainer}>
+        <div className='profile-img-wrapper'>
+          <img src={image} alt="" />
+          <span>
+            <FontAwesomeIcon icon='fa-camera'/>
+          </span>
         </div>
-        <div>
-            <h2>Lisa Richardson</h2>
-            <h5>Ethical Hacker</h5>
-        </div>
+        <input type="file"
+          accept='image/*'
+          capture="environment"
+          style={{display:'none'}}
+          ref={fileInputRef}
+          onChange={handleProfileImageChange}
+        />
+      </div>
+      <div>
+        <h2>Lisa Richardson</h2>
+        <h5>Ethical Hacker</h5>
+        {/* {errorMessage && <p className="error-message">{errorMessage}</p>} */}
+      </div>
     </div>
   )
 }

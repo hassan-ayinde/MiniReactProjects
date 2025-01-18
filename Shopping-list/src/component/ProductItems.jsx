afaf2fe3bdf59b7a41da1712c1518ab387643be6
hassan-ayinde <hassantoheebayinde@gmail.com>
2025-01-18 23:36:32 +0100

    Change the background-image

--- Shopping-list/src/component/ProductItems.jsx
+++ Shopping-list/src/component/ProductItems.jsx
@@ -15,7 +15,7 @@ const ProductItems = ({imgSrc, productTitle, productPrice}) => {
             </div>
         </div>
         <div className='flex flex-col items-end justify-evenly'>
-          <button className='text-red-500'>
+          <button className='text-slate-500'>
             <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
             </svg>

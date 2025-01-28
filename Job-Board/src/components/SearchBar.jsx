09293d664dd733ea1d136709bde06cd80e334ab0
hassan-ayinde <Hassan.toheb.ayinde@gmail.com>
2025-01-28 01:12:07 +0100

    Build: SearchBar compopont resposiveness across all screen size

--- Job-Board/src/components/SearchBar.jsx
+++ Job-Board/src/components/SearchBar.jsx
@@ -4,11 +4,14 @@ import { PiMapPinLight } from "react-icons/pi";
 
 const SearchBar = () => {
   return (
-    <div>
-        <form class="max-w-sm mx-auto flex items-center bg-gray-primary p-3 rounded-2xl">
-            <div className='flex items-center'>
-                <div>
-                    <div class="relative">
+    <div className='flex items-center h-screen justify-center'>
+        <form 
+            class="flex flex-col sm:flex-row items-center bg-gray-primary p-3 rounded-2xl gap-3 w-[90%] max-w-lg sm:w-full sm:max-w-xl transition-all duration-300 ease-linear"
+            onSubmit={(e) => e.preventDefault()}
+        >
+            <div className='flex items-center w-full'>
+                <div className='w-full'>
+                    <div class="relative w-full">
                         <div class="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
                             <IoIosSearch />
                         </div>
@@ -16,11 +19,12 @@ const SearchBar = () => {
                     </div>
                 </div>
 
-                <div className='relative'>
+                <div className='relative w-full'>
                     <div className='absolute inset-y-0 inset-x-0 start-0 ps-1 flex items-center pointer-events-none'>
                         <PiMapPinLight />
                     </div>
                     <select id="countries" class="bg-gray-50 border border-gray-300 focus:outline-none rounded-e-lg text-gray-900 text-sm block w-full ps-5 p-1.5 cursor-pointer">
+                        <option>Localization</option>
                         <option>United States</option>
                         <option>Canada</option>
                         <option>France</option>
@@ -28,8 +32,8 @@ const SearchBar = () => {
                     </select>
                 </div>
             </div>
-            <div>
-                <button type="submit" className='bg-pink-400 text-white px-3 py-1 rounded-2xl'>Search</button>
+            <div className='w-full sm:w-fit'>
+                <button type="submit" className='bg-pink-400 text-white px-3 py-1 rounded-xl w-full cursor-pointer hover:bg-pink-800'>Search</button>
             </div>
         </form>
     </div>
